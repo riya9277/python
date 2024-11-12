cb1fa3ebc40e44f1151e17e9bd033ead2538c89b
riya <riyakv2020@gmail.com>
2024-11-12 10:01:56 +0530

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,40 @@
+import random
+while True:
+    choices=["rock","paper","scissors"]
+    computer= random.choice(choices)
+    player=None
+    while player not in choices:
+        player=input()
+    
+    if(player==computer):
+        print("tie")
+    elif(player=="scissors"):
+        if(computer=="paper"):
+            print(computer)
+            print("you win")
+        if(computer=="rock"):
+            print(computer)
+            print("you loose")
+    elif(player=="paper"):
+        if(computer=="scissors"):
+            print(computer)
+            print("you loose")
+        if(computer=="rock"):
+            print(computer)
+            print("you win")
+    elif(player=="rock"):
+        if(computer=="scissors"):
+            print(computer)
+            print("you win")
+        if(computer=="paper"):
+            print(computer)
+            print("you loose")
+    playagain=input("yes or no")
+    if playagain!="yes":
+        break
+
+#    --------------------------------------------------------------
+#walrus operator
+foods=list()
+while food:=input("enter food")!="exit":
+    foods.append(food)
